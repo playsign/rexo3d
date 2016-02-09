874d6d619fcf175496933606c449a09428788015
Erno Kuusela <erno@playsign.net>
2016-02-09 16:49:22 +0200

    export-blend.bash: Resize textures for power-of-2

--- export-blend.bash
+++ export-blend.bash
@@ -6,6 +6,22 @@ command -v blender >/dev/null 2>&1 || { echo >&2 "Blender is not accessible from
 
 set -e
 
+function roundup_pot {
+    prev=1
+    for p in 2 4 8 16 32 64 128 256 512 1024 2048; do
+        if test $1 = $p; then
+            echo $p
+            return
+        fi
+        if test $1 -lt $p && test $1 -gt $prev; then
+            echo $p # change to $prev to round down (and rename function)
+            return
+        fi
+        prev=$p
+    done
+    echo $p
+}
+
 for f in "$@"; do
     blender --background "$f" --python o3d_export_blender_script.py -- \
         out.json --vertices --normals \
@@ -36,7 +52,30 @@ for f in "$@"; do
 
     test -d "$destdir/derp" && mv "$destdir/derp/"* "$destdir" && rmdir "$destdir/derp"
 
+    echo check for/fix non-power-of-2 textures
+    for f in *.png *.jpg; do
+        test -f "$f" || continue
+        arr=($(identify -format "%w %h" "$f"))
+        w=${arr[0]}
+        h=${arr[1]}
+        if test -z "$w" -o -z "$h"; then
+            echo failed to get dimensions of $f
+            exit 1
+        fi
+        neww=$(roundup_pot $w)
+        newh=$(roundup_pot $h)
+        if test $neww = $w && test $newh = $h; then
+            true
+        else
+            mkdir -p npot
+            mv "$f" npot/
+            convert -verbose -geometry ${neww}x${newh} "npot/$f" "$f"
+        fi
+            
+    done
+
     echo finished exporting $f
     echo export can be found at "$destdir"/index.html
 
 done
+
